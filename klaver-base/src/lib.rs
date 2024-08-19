17535d274f6076179eabbf156a048af37a019149
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2024-08-19 20:09:31 +0200

    export

--- klaver-base/src/lib.rs
+++ klaver-base/src/lib.rs
@@ -1,7 +1,9 @@
 mod abort_controller;
 mod blob;
 mod dom_exception;
-mod event_target;
+pub mod event_target;
 mod module;
 
 pub use module::Module;
+
+pub use self::{dom_exception::DOMException, event_target as events};
