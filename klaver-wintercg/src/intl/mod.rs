472aded14bc8aafbea958f3682f243b8cc9c0e34
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-11-22 15:22:45 +0100

    Add initial support for NumberFormat

--- klaver-wintercg/src/intl/mod.rs
+++ klaver-wintercg/src/intl/mod.rs
@@ -1,11 +1,13 @@
 mod datetime;
 mod locale;
+mod numberformat;
 pub mod provider;
 
 #[cfg(feature = "icu-compiled")]
 pub mod baked;
 
 use locale::JsLocale;
+use numberformat::NumberFormat;
 use rquickjs::{Class, Object};
 
 pub use self::datetime::DateTimeFormat;
@@ -14,6 +16,7 @@ pub fn register<'js>(ctx: &rquickjs::prelude::Ctx<'js>) -> rquickjs::Result<()> 
     let intl = Object::new(ctx.clone())?;
 
     Class::<DateTimeFormat>::define(&intl)?;
+    Class::<NumberFormat>::define(&intl)?;
     Class::<JsLocale>::define(&intl)?;
 
     ctx.globals().set("Intl", intl)?;
--- test.js
+++ test.js
@@ -1,25 +1,22 @@
 const formatter = new Intl.DateTimeFormat("en_gb", {
-	//   timeZone: "Atlantic/Reykjavik",
-	//   dateStyle: "full",
-	//   timeStyle: "full",
-	//   year: "2-digit",
-	weekday: "short",
-	day: "numeric",
-	//   month: "narrow",
-	hour: "2-digit",
-	minute: "2-digit",
-	year: "numeric",
-	month: "long",
-	timeZoneName: "longGeneric",
+  //   timeZone: "Atlantic/Reykjavik",
+  //   dateStyle: "full",
+  //   timeStyle: "full",
+  //   year: "2-digit",
+  weekday: "short",
+  day: "numeric",
+  //   month: "narrow",
+  hour: "2-digit",
+  minute: "2-digit",
+  year: "numeric",
+  month: "long",
+  timeZoneName: "longGeneric",
 });
 
 console.log(formatter.format(new Date()));
 
 // console.log(formatter.calendar());
-console.log(new Date());
 
-async function main() {
-	// await fetch("/test mig");
-	throw new Error("Helloe");
-}
-await main();
+const number = new Intl.NumberFormat("da");
+
+console.log(number.format(20202));
