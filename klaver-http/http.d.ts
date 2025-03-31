7b922bdbe054a7b4fe5460218051dc3110ae48e9
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2025-03-31 22:35:48 +0200

    Add module types

---
+++ klaver-http/http.d.ts
@@ -0,0 +1,15 @@
+export type Handler = (req: Request) => Promise<Response> | Response;
+
+export class Router {
+	get(path: string, handler: Handler): void;
+	post(path: string, handler: Handler): void;
+	patch(path: string, handler: Handler): void;
+	put(path: string, handler: Handler): void;
+	delete(path: string, handler: Handler): void;
+}
+
+export interface ServeOptions {
+	port?: number;
+}
+
+export function serve(router: Router, opts: ServeOptions): Promise<void>;
